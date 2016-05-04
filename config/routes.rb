ad5429b707bd18d4af90e27fef98bb1f10a70234
Henry23 <gabriel23rock@gmail.com>
2016-05-04 14:14:43 -0600

    routes solo para carrels y home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :hours_reserveds
-  resources :study_carrels
-  resources :students
+  resources :study_carrels, only: [:carrels]
+  resources :students, only: [:home]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
